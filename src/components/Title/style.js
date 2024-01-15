ea645240c31d00df158533501b17573c2554b5b3
PedroCastello <pedrogor@outlook.com>
2024-01-15 19:07:10 -0300

    font fix

--- src/components/Title/style.js
+++ src/components/Title/style.js
@@ -3,12 +3,9 @@ import styled from 'styled-components'
 
 export const H1 = styled.h1`
   color: #fff;
-
   text-align: center;
-  font-family: Roboto;
   font-size: 34px;
   font-weight: 700;
   line-height: normal;
-
   margin-bottom: 80px;
 `;
